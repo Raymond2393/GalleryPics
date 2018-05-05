647ef9ff76caa5c457494541299d0b76beae4ee6
Raymond Okwenda <raymondokwenda@Raymonds-MacBook-Pro.local>
2018-05-05 20:37:02 +0300

    add navbar

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    place = model.CharField(max_length=30)
+
+    def __str__(self):
+        return self.place
+        class Meta:
+            ordering = ['place']
+
+    def save_location(self):
+        self.save()
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category
+
+    def save_category(self):
+        self.save()
--- photos/views.py
+++ photos/views.py
@@ -3,3 +3,6 @@ from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to my Gallery')
+def index(request):
+    photos=Gallery.objects.all()
+    return render(request,'index.html',{"photo": photo})
