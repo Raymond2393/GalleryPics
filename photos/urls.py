441d88c801a66694f29c28f5a4c3395e155ed3e4
Raymond Okwenda <raymondokwenda@Raymonds-MacBook-Pro.local>
2018-05-07 03:10:11 +0300

    update urls

--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url(r'^$', views.index, name='index'),
+    url(r'^search/', views.search_results, name='search'),
+    url(r'^details/(\d+)',views.description,name='details'),
+    url(r'^category/', views.category, name='classfied'),
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
